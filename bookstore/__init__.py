081039b333942dedb63aad64df889c03270690af
Fernanda <fernanda.dobal@network.rca.ac.uk>
2017-12-19 21:55:23 +0000

    bookstore with tests

--- bookstore/__init__.py
+++ bookstore/__init__.py
@@ -1,1 +1,26 @@
 from .bookstore import *
+
+#create bookstore with name
+fdbookstore = Bookstore("Fernanda's Books")
+
+#add authors
+poe = Author('Edgar Allen Poe', 'UK')
+joyce = Author('James Joyce', 'IR')
+borges = Author('Jorge Luis Borges', 'AR')
+
+
+#create books
+raven = Book("The Raven", author=poe)
+valdemar = Book("Mr. Valdemar", author=poe)
+ulysses = Book("Ulysses", author=joyce)
+ficciones = Book("ficciones", author=borges)
+
+#add books to store
+fdbookstore.add_book(raven)
+fdbookstore.add_book(valdemar)
+fdbookstore.add_book(ulysses)
+fdbookstore.add_book(ficciones)
+
+#initialize
+print(fdbookstore.name)
+print(fdbookstore.get_books)
--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,51 @@
 class Bookstore(object):
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+        self.authors = []
+    #adds books to the bookstore    
+    def add_book(self, a_book):
+        self.books.append(a_book)
+        self.authors.append(a_book.author)
+    #get all books
+    def get_books(self):
+        return self.books
+    #get books by author or title     
+    def search_books(self, author=None, title=None):
+        results = []
+        if title and author:
+            for book in self.books:
+                if title.lower() in book.title.lower():
+                    results.append(book)
+            return restuls
+        if title == None:
+            for book in self.books:
+                if book.author == author:
+                    results.append(book)
+            return results
+        if author == None:
+            for book in self.books:
+                if title.lower() in book.title.lower():
+                    results.append(book)
+            return results
 
 
 class Author(object):
-    pass
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.books =[]
+    def get_books(self):
+        return self.books
+        
 
 
 class Book(object):
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        #adds book object to Author object's list of books 
+        self.author.books.append(self)
+
+
+
